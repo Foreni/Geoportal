1a85f9ce37e1fa3d64c791afa667818d3e07ec66
Foreni <michal.fudali798@gmail.com>
2021-03-27 09:33:29 +0100

    Add map to index.html. Add main .css i .js files too.

---
+++ main.js
@@ -0,0 +1,16 @@
+let map = L.map('map', {
+    dragging: false,
+    // zoomControl: false,
+    // touchZoom: false,
+    // doubleClickZoom: false,
+    // scrollWheelZoom: false,
+    minZoom: 12,
+    maxZoom: 16,
+    maxBoundsViscosity: 1
+    
+
+}).setView([49.7821562, 22.7692634], 14);
+
+map.setMaxBounds(map.getBounds());
+
+L.tileLayer('https://b.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
