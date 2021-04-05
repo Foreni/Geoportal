d3020d23c9ad01931cb9b5449396e60559b200aa
Foreni <michal.fudali798@gmail.com>
2021-04-05 23:00:26 +0200

    Add sensors' symbols to the map with colors depending on a PM10 pollution level.

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
+let activeSensorLayerID = undefined;
+
+function getPM10Color(d) {
+    if (d === undefined) return '#bfbfbf';
+    return d > 150 ? '#990000' :
+           d > 110  ? '#e50000' :
+           d > 80  ? '#e58100' :
+           d > 50  ? '#ffd911' :
+           d > 20  ? '#b0dd10' : '#57b108'
+}
+
 let map = L.map('map', {
-    dragging: true,
     minZoom: 11,
     maxZoom: 16,
+    doubleClickZoom: false
 
 }).setView([49.7821562, 22.7692634], 11);
 
 map.setMaxBounds(map.getBounds());
 
-L.tileLayer('https://b.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer(atob("aHR0cHM6Ly97c30uYmFzZW1hcHMuY2FydG9jZG4uY29tL3Jhc3RlcnRpbGVzL2RhcmtfYWxsL3t6fS97eH0ve3l9LnBuZw=="), {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a><br>Measurements: <a href="https://sensor.community/">Sensor.Community</a> contributors.'
 }).addTo(map);
 
-L.geoJSON(data, {
-    style: function(feature) {
-        return {
-            color: '#a17d9d',
-            fillOpacity: 0.2
-        };
-    }
-}).addTo(map)
-
-let radius = 1; //kilometers
+let radius = 50; //kilometers
 let requestUrl = 'http://data.sensor.community/airrohr/v1/filter/area=49.7821562,22.7692634,' + radius.toString();
 let request = new XMLHttpRequest();
 request.open('GET', requestUrl);
@@ -28,7 +30,7 @@ request.responseType = 'json';
 
 //TUTAJ WSADZAĆ KOD WYKONUJĄCY SIĘ PRZY POKAZYWANIU CZUJNIKÓW NA MAPIE
 request.onload = () => {
-    document.getElementById("preloader").remove();
+    document.getElementById("preloader").hidden = true;
     const response = request.response;
     sensors = {};
     sensors.type = 'FeatureCollection';
@@ -61,41 +63,69 @@ request.onload = () => {
         }
     }
 
-    // let interpolation = turf.interpolate(sensors, 1, {
-    //     property: 'sensordatavalues.id',
-    //     gridType: 'hex'
-    // });
+    let normalRadius = 8;
+    let markedRadius = 15;
+    let markedStyles = {
+        stroke: true
+    };
 
-    // L.geoJSON(interpolation, {
-    //     onEachFeature: (feature, layer) => {layer.bindPopup(feature.properties.id.toString())} 
-    // }).addTo(map);
+    function markedSensorStyling () {
+        markers._layers[activeSensorLayerID]._radius = normalRadius
+    }
 
-    console.log(sensors.features);
-    L.geoJSON(sensors).addTo(map);
+    markers = L.geoJSON(sensors, {
+        pointToLayer: (geoJsonPoint, latlng) => {
+            return L.circleMarker(latlng, {"radius": normalRadius})
+        },
+        style: (feature) => {
+            featureStyle = {
+                stroke: false,
+                color: "white",
+                weight: 2,
+                fillOpacity: 1,
+                fillColor: '#bfbfbf'
+            }
+
+            if (feature.properties.SDS011 !== undefined)
+                featureStyle.fillColor = getPM10Color(feature.properties.SDS011.sensordatavalues[0].value);
+            return featureStyle;
+        },
+        onEachFeature: (feature, layer) => {
+            layer.on('click', () => {
+                if (activeSensorLayerID !== undefined) {
+                    markedSensorStyling(); markers.resetStyle();
+                }
+                
+                if (activeSensorLayerID !== layer._leaflet_id) {
+                    layer._radius = markedRadius;
+                    layer.setStyle(markedStyles)
+                    activeSensorLayerID = layer._leaflet_id;
+                }
+
+                else {
+                    activeSensorLayerID = undefined;
+                }
+            });
+        }
+    });
+
+    markers.addTo(map);
 }
 
 request.send();
 
+//MAP KEY BELOW
+
 var legend = L.control({position: 'bottomleft'});
-function getColor(d) {
-    return d > 500 ? '#800080' :
-           d > 200  ? '#BD0026' :
-           d > 100  ? '#E31A1C' :
-           d > 50  ? '#ff3300' :
-           d > 25   ? '#ffb31a' :
-                      '#00ff00';
-}
+
 legend.onAdd = function (map) {
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 25, 50, 100, 200, 500],
-        labels = [];
+    var div = L.DomUtil.create('div', 'legend'),
+    grades = [0, 20, 50, 80, 110, 150]
 
-    // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        '<i style="background:' + getPM10Color(grades[i] + 1) + '"></i> ' + (grades[i] != 0 ? (grades[i] + 0.1).toString().replace(".", ",") : grades[i]) + (grades[i + 1] ? ' &ndash; ' + grades[i + 1]+ '<br>' : '+');
     }
 
     return div;
